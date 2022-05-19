c05c821f8dc13ddac4fe50fb07798211aeb39a7c
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-19 12:56:10 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Courseentry from './component/Courseentry';
+import Viewcouse from './component/Viewcouse';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Courseentry/>
-    
+    <Viewcouse/>
+
+
+
     </>
 
   );
